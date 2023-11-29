30759c1b800a631995e2481c5080f7dd90b7d117
Jennifer Lynn Smith <jennifer@rocktownagency.com>
2023-11-29 14:22:24 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Multiply num1 and num2
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Calculate the remainder
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+// Find the maximum value
+const max = Math.max(20, 5, 10, 15, 20);
